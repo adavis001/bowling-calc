ad46942003ff7f901fd7e02358acb039034d8133
Alexander Davis <akonidavis@gmail.com>
2017-03-31 19:43:20 -1000

    everything but the last frame

--- bowlingScore.js
+++ bowlingScore.js
@@ -2,17 +2,30 @@
 
 var bowlingScore = function(arr){
 	var score = 0;
-	for(i=0; i<arr.length; i++){
+	for(i=0; i<=9; i++){
+	if (arr[i].b1 <9 && (arr[i].b1 + arr[i].b2 === 10)){
+		//score += arr[i].b1 + arr[i].b2;
+		console.log("Spare");
+		//console.log(score);
+		score += (arr[i+1].b1);
+	} if (arr[i].b1 === 10 && arr[i].b2 === 0){
+		console.log("Strike!!");
+		score+=(arr[i+1].b1);
+		score+=(arr[i+1].b2);
 		score += arr[i].b1 + arr[i].b2;
-		}
-
+		// console.log(score);
+	} if(arr[i]===arr[9]){
+		
+	}else {
+		score += arr[i].b1 + arr[i].b2;
+		// console.log("Spareless");
+		// console.log(score);
+	}
+}
+ score += arr[9].b1 +arr[9].b2;
 	return score;
 };
 
 
-var framesKaleo = [{b1:8,b2:2},{b1:1, b2:8}];
-var framesAlex =  [{b1:2,b2:3},{b1:3, b2:6}];
-
 module.exports = bowlingScore;
 
-console.log(bowlingScore(framesKaleo));
--- tests/bowlingScore.spec.js
+++ tests/bowlingScore.spec.js
@@ -3,17 +3,31 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const bowlingScore = require('../bowlingScore.js');
-var framesKaleo = [{b1:8,b2:2},{b1:1, b2:8}];
-var framesAlex =  [{b1:2,b2:3},{b1:3, b2:6}];
+var framesKaleo = [{b1:8,b2:1},{b1:1, b2:8}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesAlex = [{b1:2,b2:8},{b1:3, b2:6}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesAndrew = [{b1:10,b2:0},{b1:2, b2:6}, {b1:2,b2:2},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesErnest = [{b1:10,b2:0},{b1:3, b2:6}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesGagan = [{b1:0,b2:0},{b1:0, b2:0}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesJasmine =  [{b1:10,b2:0},{b1:3, b2:6},{b1:1, b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1,b3:0}];
 
 describe('bowlingScore', ()=> {
 	it('should be a function', function(){
 		expect(bowlingScore).to.be.a('function');
 	});
-	it('should return 10 if spare', function(){
-		expect(bowlingScore(framesKaleo)).to.be.equal(19);
+	it('should add score if not a spare', function(){
+		expect(bowlingScore(framesKaleo)).to.be.equal(18);
 	});
 	it('should return 0 and bonus if fail', function(){
-		expect(bowlingScore(0,0)).to.be.equal(0);
+		expect(bowlingScore(framesGagan)).to.be.equal(0);
+	});
+	it('should add bonus points for spare', function(){
+		expect(bowlingScore(framesAlex)).to.be.equal(22);
+	});
+	it('should add DOUBLE BONUS points for a strike', function(){
+		expect(bowlingScore(framesErnest)).to.be.equal(28);
+		expect(bowlingScore(framesAndrew)).to.be.equal(30);
+	});
+	it('should give frame 10 Two or Three throws', function(){
+		expect(bowlingScore(framesJasmine)).to.be.equal(44);
 	});
 });
