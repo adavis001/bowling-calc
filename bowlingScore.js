1c13d71281801dd012529fc4078a5ae2fb922e82
Alexander Davis <akonidavis@gmail.com>
2017-04-01 10:22:36 -1000

    lets bowl!

--- bowlingScore.js
+++ bowlingScore.js
@@ -2,27 +2,34 @@
 
 var bowlingScore = function(arr){
 	var score = 0;
-	for(i=0; i<=9; i++){
-	if (arr[i].b1 <9 && (arr[i].b1 + arr[i].b2 === 10)){
+
+
+	for(i=0; i<arr.length; i++){
+	if (i< 8 && arr[i].b1 <9 && (arr[i].b1 + arr[i].b2 === 10)){
+			// if(arr[i] === arr[9]){
+			// 	score += arr[i].b1 + arr[i].b2 + arr[i].b3;
+			// }
 		//score += arr[i].b1 + arr[i].b2;
-		console.log("Spare");
+		//console.log("Spare");
 		//console.log(score);
 		score += (arr[i+1].b1);
-	} if (arr[i].b1 === 10 && arr[i].b2 === 0){
-		console.log("Strike!!");
+	} 
+	if (i< 8 && arr[i].b1 === 10 && arr[i].b2 === 0){
+		//console.log("Strike!!");
 		score+=(arr[i+1].b1);
 		score+=(arr[i+1].b2);
-		score += arr[i].b1 + arr[i].b2;
 		// console.log(score);
-	} if(arr[i]===arr[9]){
-		
-	}else {
-		score += arr[i].b1 + arr[i].b2;
-		// console.log("Spareless");
-		// console.log(score);
+	//} 
+	// if(arr[i]===arr[9]){
+	// 	score += arr[i].b1 + arr[i].b2 + arr[i].b3;
 	}
+	if (i === 9) {
+		console.log(score);
+		score+= arr[9].b3;
+	}
+		score += arr[i].b1 + arr[i].b2;
+		console.log("Frame" + (i+1));
 }
- score += arr[9].b1 +arr[9].b2;
 	return score;
 };
 
--- tests/bowlingScore.spec.js
+++ tests/bowlingScore.spec.js
@@ -5,10 +5,10 @@ const expect = chai.expect;
 const bowlingScore = require('../bowlingScore.js');
 var framesKaleo = [{b1:8,b2:1},{b1:1, b2:8}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
 var framesAlex = [{b1:2,b2:8},{b1:3, b2:6}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
-var framesAndrew = [{b1:10,b2:0},{b1:2, b2:6}, {b1:2,b2:2},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
+var framesAndrew = [{b1:10,b2:0},{b1:2, b2:6}, {b1:2,b2:2},{b1:1,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
 var framesErnest = [{b1:10,b2:0},{b1:3, b2:6}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
 var framesGagan = [{b1:0,b2:0},{b1:0, b2:0}, {b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0},{b1:0,b2:0,b3:0}];
-var framesJasmine =  [{b1:10,b2:0},{b1:3, b2:6},{b1:1, b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1,b3:0}];
+var framesJasmine =  [{b1:10,b2:0},{b1:3, b2:6},{b1:1, b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:1,b2:1},{b1:8,b2:2,b3:1}];
 
 describe('bowlingScore', ()=> {
 	it('should be a function', function(){
@@ -25,9 +25,18 @@ describe('bowlingScore', ()=> {
 	});
 	it('should add DOUBLE BONUS points for a strike', function(){
 		expect(bowlingScore(framesErnest)).to.be.equal(28);
-		expect(bowlingScore(framesAndrew)).to.be.equal(30);
+		expect(bowlingScore(framesAndrew)).to.be.equal(31);
 	});
 	it('should give frame 10 Two or Three throws', function(){
-		expect(bowlingScore(framesJasmine)).to.be.equal(44);
+		expect(bowlingScore(framesJasmine)).to.be.equal(53);
 	});
 });
+	
+		// function lastFrame(arr){
+		// if(arr[9].b1 <9 && (arr[9].b1 + arr[9].b2 === 10)){
+		// 	console.log(arr[9].b1);
+		// 	console.log(arr[9].b2);
+		// 	console.log(arr[9].b3);
+		// 	console.log("Frame 10!");
+		// 	score += arr[9].b1 + arr[9].b2 + arr[9].b3;
+		// }
